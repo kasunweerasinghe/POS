91cbba1cbdadeca434a8d234ce6de5312cbcb073
kasun <https://github.com/GDSE-59-60-Project-Zone/Validations.git>
2022-05-02 15:17:30 +0530

    load all items

---
+++ Layered_Architecture/src/dao/ItemDAOImpl.java
@@ -0,0 +1,31 @@
+package dao;
+
+import db.DBConnection;
+import model.ItemDTO;
+import view.tdm.ItemTM;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class ItemDAOImpl {
+    //load All Items
+    public ArrayList<ItemDTO> loadAllItems() throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Item");
+        ArrayList<ItemDTO> allItem = new ArrayList<>();
+
+        while (rst.next()) {
+            String code = rst.getString(1);
+            String description = rst.getString(2);
+            BigDecimal unitPrice = rst.getBigDecimal(3);
+            int qtyOnHand = rst.getInt(4);
+        }
+        return allItem;
+    }
+
+}
