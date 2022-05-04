2c62406312a419537e5da5618eec9cc190883e28
kasun <https://github.com/GDSE-59-60-Project-Zone/Validations.git>
2022-05-04 09:07:42 +0530

    Refactored isExistsItem

--- Layered_Architecture/src/dao/PlaceOrderDAOImpl.java
+++ Layered_Architecture/src/dao/PlaceOrderDAOImpl.java
@@ -16,17 +16,21 @@ public class PlaceOrderDAOImpl {
     }
 
     public boolean isExistsItem(String code) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("SELECT code FROM Item WHERE code=?");
-        pstm.setString(1, code);
-        return pstm.executeQuery().next();
+//        Connection connection = DBConnection.getDbConnection().getConnection();
+//        PreparedStatement pstm = connection.prepareStatement("SELECT code FROM Item WHERE code=?");
+//        pstm.setString(1, code);
+//        return pstm.executeQuery().next();
+
+        return SQLUtil.executeQuery("SELECT code FROM Item WHERE code=?",code).next();
     }
 
     public boolean isExistsCustomer(String id) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("SELECT id FROM Customer WHERE id=?");
-        pstm.setString(1, id);
-        return pstm.executeQuery().next();
+//        Connection connection = DBConnection.getDbConnection().getConnection();
+//        PreparedStatement pstm = connection.prepareStatement("SELECT id FROM Customer WHERE id=?");
+//        pstm.setString(1, id);
+//        return pstm.executeQuery().next();
+
+        return SQLUtil.executeQuery("SELECT id FROM Customer WHERE id=?",id).next();
     }
 
     public String generateOrderID() throws SQLException, ClassNotFoundException {
@@ -35,13 +39,11 @@ public class PlaceOrderDAOImpl {
         ResultSet rst = stm.executeQuery("SELECT oid FROM `Orders` ORDER BY oid DESC LIMIT 1;");
 
         return rst.next() ? String.format("OID-%03d", (Integer.parseInt(rst.getString("oid").replace("OID-", "")) + 1)) : "OID-001";
+
     }
 
     public ArrayList<String> loadAllCustomerIds() throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        Statement stm = connection.createStatement();
-        ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
-
+        ResultSet rst = SQLUtil.executeQuery("SELECT * FROM Customer");
         ArrayList<String> allCustomerID = new ArrayList<>();
 
         while (rst.next()){
@@ -52,9 +54,7 @@ public class PlaceOrderDAOImpl {
     }
 
     public ArrayList<String> loadAllItemCodes() throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        Statement stm = connection.createStatement();
-        ResultSet rst = stm.executeQuery("SELECT * FROM Item");
+        ResultSet rst = SQLUtil.executeQuery("SELECT * FROM Item");
         ArrayList<String> loadAllItemCode = new ArrayList<>();
 
         while (rst.next()){
