7283a4675d64e150b74af2f5a21db3893eac5aee
kasun <https://github.com/GDSE-59-60-Project-Zone/Validations.git>
2022-05-10 10:37:33 +0530

    add CustomDTO

--- Layered_Architecture/src/dao/Custom/QueryDAO.java
+++ Layered_Architecture/src/dao/Custom/QueryDAO.java
@@ -1,7 +1,10 @@
 package dao.Custom;
 
+import model.CustomDTO;
+
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface QueryDAO {
-     void searchOrderByOrderID(String ID) throws SQLException, ClassNotFoundException;
+     ArrayList<CustomDTO> searchOrderByOrderID(String ID) throws SQLException, ClassNotFoundException;
 }
